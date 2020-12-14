9cb4d2cf3644c29ff6fcfecb046cf75cf776107e
Лия <32329126+liya19@users.noreply.github.com>
2020-12-14 15:38:26 +0300

    Update haara.py

--- 12. haara/haara.py
+++ 12. haara/haara.py
@@ -20,9 +20,8 @@ faces = faceCascade.detectMultiScale(
     scaleFactor=1.1,
     minNeighbors=5,
     flags=cv2.CASCADE_SCALE_IMAGE
-)  # For each face
+
 for (x, y, w, h) in faces:
-    # Draw rectangle around the face
     cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 255, 255), 3)
 
 plt.figure(figsize=(12, 8))
